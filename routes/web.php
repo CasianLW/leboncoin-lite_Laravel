c9499f56af35450a4d57410fb3c7cd0437f0b550
CasianLW <ciorbacasian@gmail.com>
2023-03-10 16:52:58 +0100

    routes of the project (map of the controller)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AnnonceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('annonces', AnnonceController::class);
+
+Route::get('/', [AnnonceController::class, 'index']);
+// Route::get('/', function () {
+//     return view('welcome');
+// });
