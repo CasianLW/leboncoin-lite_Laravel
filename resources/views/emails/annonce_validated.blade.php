c771f142de54937112e6f7e78e6282fb04fe210e
CasianLW <ciorbcasian@gmail.com>
2023-03-10 19:54:42 +0100

    view of the confirmed annonce mail

---
+++ resources/views/emails/annonce_validated.blade.php
@@ -0,0 +1,16 @@
+    
+    <br>
+    <br>
+    <br>
+    <h1>Bonjour {{ $annonce->name }},</h1>
+
+<h2>Votre annonce a bien été mise en ligne: "{{ $annonce->title }}".</h2>
+
+<p>Pour supprimer l'annonce, cliquez sur le lien suivant:</p>
+<a href="{{ route('annonces-delete.destroy', $annonce->token) }}">{{ route('annonces-delete.destroy', $annonce->token) }}</a>
+
+
+<br>
+<br>
+Cordialement,
+L'équipe {{ config('app.name') }}
