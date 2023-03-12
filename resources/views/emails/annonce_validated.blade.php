6904bb2cdf1f0d826be720ddd6a4b4e3039917d2
CasianLW <ciorbacasian@gmail.com>
2023-03-12 18:09:03 +0100

    modif annonce link

--- resources/views/emails/annonce_validated.blade.php
+++ resources/views/emails/annonce_validated.blade.php
@@ -1,11 +1,17 @@
     
-    <br>
+
+ <br>
     <br>
     <br>
     <h1>Bonjour {{ $annonce->name }},</h1>
 
 <h2>Votre annonce a bien été mise en ligne: "{{ $annonce->title }}".</h2>
 
+<p>Pour mofifier l'annonce, cliquez sur le lien suivant:</p>
+<a href="{{ route('annonces.edit', $annonce->id) }}">Modifier l'annonce</a>
+
+<br>
+<br>
 <p>Pour supprimer l'annonce, cliquez sur le lien suivant:</p>
 <a href="{{ route('annonces-delete.destroy', $annonce->token) }}">{{ route('annonces-delete.destroy', $annonce->token) }}</a>
 
